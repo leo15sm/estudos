810a2054209ef1eace3153162de20f2bbef96642
leo15sm <nickname.player15@gmail.com>
2023-05-17 14:54:46 -0300

    galeria criada mas sem suavisação na trasição

--- js/teste.js
+++ js/teste.js
@@ -9,3 +9,32 @@ function teste(){
 }
 
 window.addEventListener('scroll', teste);
+
+/*############################################################################################*/
+let time=5000; 
+let index=0;
+const images = document.querySelectorAll("#galeria figure img");
+const max = images.length;
+    
+function nextImg(){
+    images[index].classList.add("inativo");
+    index++;
+
+    if(index >= max){
+        index=0;
+    }
+
+    images[index].classList.remove("inativo");
+}
+
+function start (){
+    setInterval(()=> {
+    //troca de imagem
+        nextImg()
+    }, time)
+}
+
+window.addEventListener('load', start);
+
+
+
